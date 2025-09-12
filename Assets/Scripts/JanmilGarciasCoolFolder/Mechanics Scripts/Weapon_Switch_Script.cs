694d12a23c80e8d2a4df29e8e7f17438c3d00fb4
Jan <152419919+itsJanIg@users.noreply.github.com>
2025-09-12 14:16:08 -0500

    Update Weapon_Switch_Script.cs
    Done, added the HL weapon shift with above qwerty 1,2,3 etc

--- Assets/Scripts/JanmilGarciasCoolFolder/Mechanics Scripts/Weapon_Switch_Script.cs
+++ Assets/Scripts/JanmilGarciasCoolFolder/Mechanics Scripts/Weapon_Switch_Script.cs
@@ -14,10 +14,9 @@ public class Weapon_Switch_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         int previousSelectedWeapon = selectedWeapon;
 
-        if (Input.GetAxis("Mouse Scrollwheel") > 0f)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             if (selectedWeapon >= transform.childCount - 1)
                 selectedWeapon = 0;
@@ -25,12 +24,32 @@ public class Weapon_Switch_Script : MonoBehaviour
                 selectedWeapon++;
         }
 
-        if (Input.GetAxis("Mouse Scrollwheel")<0f)
+        if (Input.GetAxis("Mouse ScrollWheel")<0f)
         {
             if (selectedWeapon <= 0)
                 selectedWeapon = transform.childCount - 1;
             else
                 selectedWeapon--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
         }
 
         if (previousSelectedWeapon !=selectedWeapon)
